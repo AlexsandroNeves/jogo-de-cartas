1ac26991e9c477c75adeabd26f30ac09c3f3527a
Alexsandro Neves <alexsandrodnss@gmail.com>
2022-08-03 16:35:15 -0300

    subindo a parte do responsivo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Menu from './Components/menu/Menu'
 import Home from './pages/home/Home'
-import { Contato } from './pages/contato/Contato'
 import { JogoCartas } from './pages/jogo/JogoCartas'
 import { useState } from 'react'
 
